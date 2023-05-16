c90f1c22ec9b7464fadd122a392b2ecbdc0aa7cc
Roberto Ludwig <robertoludwig@MacBook-Pro-de-Roberto.local>
2023-05-15 21:37:11 -0300

    category tests

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,76 @@
 import {Category} from "./category";
 
-describe("Category Tests", () => {
+describe("Category Unit Tests", () => {
+
     test("constructor of category", () => {
-        const category = new Category('Movie');
-        expect(Category.name).toBe('Movie');
+        // Arrange
+        let props = {
+            name: "Movie",
+            description: "some description",
+            is_active: true,
+            created_at: new Date(),
+        }
+
+        // Act
+        let category = new Category(props);
+
+        // Assert
+        expect(category.name).toBe('Movie');
+        expect(category.description).toBe("some description");
+        expect(category.is_active).toBeTruthy();
+        expect(category.created_at).toBe(category.props.created_at);
+        expect(category.props.created_at).toBeInstanceOf(Date);
+
+        // é possível usar a comparação exata do objeto
+        expect(category.props).toStrictEqual(props);
+
+
+        // mais formas de testar
+        category = new Category({
+            name: "Movie"
+        });
+        category['description'] = "other description";
+        expect(category.description).toBe("other description");
+
+        category['description'] = undefined;
+        expect(category.description).toBeNull();
+        category['description'] = null;
+        expect(category.description).toBeNull();
+
     });
+
+    test("getter and setter of is_active prop", () => {
+        let category = new Category({
+            name: "Movie"
+        });
+        expect(category.is_active).toBeTruthy();
+
+        category = new Category({
+            name: "Movie",
+            is_active: true
+        });
+        expect(category.is_active).toBeTruthy();
+
+        category = new Category({
+            name: "Movie",
+            is_active: false
+        });
+        expect(category.is_active).toBeFalsy();
+    });
+
+    test("getter and setter of created_at prop", () => {
+        let category = new Category({
+            name: "Movie"
+        });
+        expect(category.created_at).toBeInstanceOf(Date);
+
+        let created_at = new Date();
+        category = new Category({
+            name: "Movie",
+            created_at: created_at
+        });
+        expect(category.created_at).toBe(created_at);
+
+    });
+
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,6 +1,36 @@
-class Category {
-    constructor(public name: string) {
-        
+export type CategoryProperties = {
+    name: string, 
+    description?: string, 
+    is_active: boolean, 
+    created_at?: Date
+}
+
+// entidade - identidade, comportamentos e atributos
+// id auto incremento
+// politica e detalhes
+// UUID - Univesally Unique Identifier v4 - IETF RFC
+
+export class Category {
+    public readonly id: string;
+    constructor(public readonly props: CategoryProperties, id?: string) {
+        this.id = id
+        this.description = this.props.description;
+        this.is_active = this.props.is_active;
+        // data não precisa ter setter
+        this.props.created_at = this.props.created_at ?? new Date();
+    }
+
+    get name() { return this.props.name; }
+    get description() { return this.props.description; }
+    get is_active() { return this.props.is_active; }
+    get created_at() { return this.props.created_at; }
+
+    private set description(value: string) {
+        this.props.description = value ?? null;
+    }
+
+    private set is_active(value: boolean) {
+        this.props.is_active = value ?? true;
     }
 }
 
